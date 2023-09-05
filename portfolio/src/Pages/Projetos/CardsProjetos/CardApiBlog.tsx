0772c2e9e8b8228f5eb4a34948d1bef4887cb18b
DaniloDeV90 <duhflores78@gmail.com>
2023-09-05 14:18:04 -0300

    correcoes

--- portfolio/src/Pages/Projetos/CardsProjetos/CardApiBlog.tsx
+++ portfolio/src/Pages/Projetos/CardsProjetos/CardApiBlog.tsx
@@ -4,7 +4,7 @@ import imgBlog from "./../allProjects/imgs/imgBlog.png"
 
 const CardApiBlog = () => {
     const CardBlog =  {
-        Title: "Blod Node",
+        Title: "Blog Node",
         imagem: imgBlog,
         desc: `Esta api segue o padrão MVC, é um blog criado em Node com express e é uma api com views integrada, onde a partir você do momento em que você der start no servidor, ela cria todo o ambiente visual em localhost. 
         A api possui rotas privadas onde apenas o dono do blog pode acessar.`,
